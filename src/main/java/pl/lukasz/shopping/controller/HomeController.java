03b7fd611b9407216e6f01f6eddb42455a0b2a43
lukasz137test <mufasa137@wp.pl>
2018-09-27 13:04:27 +0200

    Created HomeController class

---
+++ src/main/java/pl/lukasz/shopping/controller/HomeController.java
@@ -0,0 +1,28 @@
+package pl.lukasz.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.lukasz.shopping.model.User;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String all(Model model) {
+        model.addAttribute("user",new User());
+        return "index";
+    }
+    @GetMapping("/home")
+    public String userHomePage(){
+        return "home";
+    }
+
+
+    @RequestMapping("/-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "index";
+    }
+}
